bb59d4b385328ae5ab364b65e6522cf8eece531e
Nichakul Pichitwutikorn <Nichakul.p@DW000460.local>
2023-06-01 16:44:33 +0700

    Add edit the book information page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,24 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description, :release)
+  end
 end
