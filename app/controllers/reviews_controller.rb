6e4745d6f8d31274333df6fbfdb35914af82f97e
Nichakul Pichitwutikorn <Nichakul.p@DW000460.local>
2023-06-01 19:39:53 +0700

    Add edit review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def edit
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :star, :book)
+  end
 end
