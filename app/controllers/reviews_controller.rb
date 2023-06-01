979dd1b87fc41f4c3da2c0739b9616aa0fa2cf7b
Nichakul Pichitwutikorn <Nichakul.p@DW000460.local>
2023-06-01 22:21:18 +0700

    Add delete button at each review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+  def destroy
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.find(params[:id])
+    @review.destroy
+
+    redirect_to book_path(@book)
+  end
 end
