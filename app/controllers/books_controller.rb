77816d29fc911c367dafc804233d006930bdc7d3
giftnichakul <giftnichakul@gmail.com>
2023-06-04 14:07:31 +0700

    Add before_action in books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update]
+
   def index
     @books = Book.all
   end
 
-  def show
+  def show; end
+
+  private
+
+  def set_book
     @book = Book.find(params[:id])
   end
 end
