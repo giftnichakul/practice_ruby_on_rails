d3fe93aeb0c3865d1bdaf84d8646f29641c1706c
giftnichakul <giftnichakul@gmail.com>
2023-06-07 15:32:14 +0700

    Show current user in every page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,7 +41,7 @@ class BooksController < ApplicationController
   private
 
   def set_book
-    @book = current_user.books.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def book_params
